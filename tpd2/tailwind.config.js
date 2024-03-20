44a6ab25d0d06da51dec4e872c9c76b83bf032da
MRINMOY MONDAL <mm2922894@gmail.com>
2024-03-21 02:36:27 +0530

    added theme supoort

--- tpd2/tailwind.config.js
+++ tpd2/tailwind.config.js
@@ -1,17 +1,26 @@
+import { createThemes } from "tw-colors";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "orange-600",
-      secondary: "zinc-800",
-    },
     extend: {
       animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      }
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    createThemes({
+      light: {
+        primary: "orange-600",
+        secondary: "zinc-200",
+        brand: "#ff0000",
+      },
+      dark: {
+        primary: "orange-600",
+        secondary: "zinc-800",
+        brand: "#ff00ff",
+      },
+    }),
+  ],
+};
